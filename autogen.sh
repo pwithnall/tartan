edf73b108d9f43c1250c069ecb9ec626d8bf8a2b
Philip Withnall <philip.withnall@collabora.co.uk>
2014-05-07 10:50:05 +0100

    build: Remove dependency on gnome-common
    Inline the relevant code from gnome-autogen.sh (there isn’t much of it),
    and do the same with the GNOME macros in configure.ac.
    
    Copy the gnome-compiler-flags.m4 macro in-tree. It’s licenced under
    GPLv2+, which is fine.

--- autogen.sh
+++ autogen.sh
@@ -7,13 +7,31 @@ test -z "$srcdir" && srcdir=.
 PKG_NAME=tartan
 
 (test -f $srcdir/configure.ac) || {
-    echo "**Error**: Directory "\`$srcdir\'" does not look like the top-level $PKG_NAME directory"
-    exit 1
-}
-
-which gnome-autogen.sh || {
-	echo "You need to install gnome-common from GNOME Git"
+	echo "**Error**: Directory "\`$srcdir\'" does not look like the top-level $PKG_NAME directory"
 	exit 1
 }
 
-REQUIRED_PKG_CONFIG_VERSION=0.17.1 REQUIRED_AUTOMAKE_VERSION=1.12 USE_GNOME2_MACROS=1 . gnome-autogen.sh "$@"
+if [ "$#" = 0 -a "x$NOCONFIGURE" = "x" ]; then
+	echo "**Warning**: I am going to run \`configure' with no arguments."
+	echo "If you wish to pass any to it, please specify them on the"
+	echo \`$0\'" command line."
+	echo
+fi
+
+# if the AC_CONFIG_MACRO_DIR() macro is used, create that directory
+# This is a automake bug fixed in automake 1.13.2
+# See http://debbugs.gnu.org/cgi/bugreport.cgi?bug=13514
+m4dir=`autoconf --trace 'AC_CONFIG_MACRO_DIR:$1'`
+if [ -n "$m4dir" ]; then
+	mkdir -p $m4dir
+fi
+
+intltoolize --force --copy --automake || exit 1
+autoreconf --verbose --force --install -Wno-portability || exit 1
+
+if test x$NOCONFIGURE = x; then
+	echo "Running $srcdir/configure " "$@" "…"
+	$srcdir/configure "$@" && echo "Now type \`make\' to compile $PKG_NAME" || exit 1
+else
+	echo "Skipping configure process."
+fi
