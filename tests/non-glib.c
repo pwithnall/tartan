2b4bc24677474e24ddf7668ec0fe39ad1c6f6622
Philip Withnall <philip.withnall@collabora.co.uk>
2014-11-20 10:00:40 +0000

    tests: Add a non-GLib test
    As a follow up to commit 915a744037d93abfcaab8b35e98e281cf88bb132, add a
    non-GLib test, which checks that scanning a file which doesn’t include
    glib.h succeeds.

---
+++ tests/non-glib.c
@@ -0,0 +1,10 @@
+/* Template: generic-non-glib */
+
+/*
+ * No error
+ */
+{
+	// This should compile OK without the checker falling over because
+	// glib.h has not been included.
+	printf ("Hello world!\n");
+}
